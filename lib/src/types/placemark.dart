e6d10106720bd550de9d51a09b802b82afe67f24
Александр Лопатка <alexanderlopatka@gmail.com>
2021-06-22 07:22:26 +0300

    Add copyWith

--- lib/src/types/placemark.dart
+++ lib/src/types/placemark.dart
@@ -10,4 +10,13 @@ class Placemark {
   final Point point;
   final PlacemarkStyle style;
   final TapCallback<Placemark, Point> onTap;
+
+  Placemark copyWith(
+          {Point point,
+          PlacemarkStyle style,
+          TapCallback<Placemark, Point> onTap}) =>
+      Placemark(
+          point: point ?? this.point,
+          style: style ?? this.style,
+          onTap: onTap ?? this.onTap);
 }
